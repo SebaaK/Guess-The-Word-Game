46513a228cf353da21043b8ae113ba6b423c0452
SebaaK <sebaa.kot@gmail.com>
2022-07-11 15:44:08 +0200

    Init project

---
+++ src/main/java/kots/JcGuessTheWordGameApplication.java
@@ -0,0 +1,13 @@
+package kots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JcGuessTheWordGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JcGuessTheWordGameApplication.class, args);
+    }
+
+}
