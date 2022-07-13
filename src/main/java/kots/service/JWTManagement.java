d248464dfe5c6a495e9d035c4aa4f4f487637969
SebaaK <sebaa.kot@gmail.com>
2022-07-13 15:45:22 +0200

    Delete unused imports

--- src/main/java/kots/service/JWTManagement.java
+++ src/main/java/kots/service/JWTManagement.java
@@ -3,7 +3,6 @@ package kots.service;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
-import kots.model.Role;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
