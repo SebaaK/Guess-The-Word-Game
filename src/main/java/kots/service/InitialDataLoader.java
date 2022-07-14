f6e0a49e8e1c490a32fa4201d1512e571c4f57db
SebaaK <sebaa.kot@gmail.com>
2022-07-14 09:40:36 +0200

    Small amendments in endpoint. Search by wordName

--- src/main/java/kots/service/InitialDataLoader.java
+++ src/main/java/kots/service/InitialDataLoader.java
@@ -58,7 +58,6 @@ class InitialDataLoader {
         userService.saveRole(new Role(2L, "ADMIN"));
         userService.saveUser(new User(2L, "sebastian_admin", passwordEncoder.encode("password"), new ArrayList<>()));
         userService.addRoleToUser("sebastian_admin", "ADMIN");
-        userService.addRoleToUser("sebastian_admin", "USER");
     }
 
     private Word completeEntityToSave(String[] splitRow) {
