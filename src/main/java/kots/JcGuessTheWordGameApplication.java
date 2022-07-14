064d8d77d2e6de903b8ba435d88db029335ed829
SebaaK <sebaa.kot@gmail.com>
2022-07-14 09:33:20 +0200

    PreAuthorize some endpoints allow only admin user

--- src/main/java/kots/JcGuessTheWordGameApplication.java
+++ src/main/java/kots/JcGuessTheWordGameApplication.java
@@ -2,8 +2,10 @@ package kots;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class JcGuessTheWordGameApplication {
 
     public static void main(String[] args) {
--- src/main/java/kots/controller/WordController.java
+++ src/main/java/kots/controller/WordController.java
@@ -8,6 +8,7 @@ import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -41,12 +42,14 @@ public class WordController {
     }
 
     @PostMapping(consumes = "multipart/form-data")
+    @PreAuthorize("hasAuthority('ADMIN')")
     public ResponseEntity<WordMetadataDto> uploadWord(@RequestParam MultipartFile file,
                                                       @RequestParam("wordName") String wordName) throws IOException {
         return ResponseEntity.status(HttpStatus.CREATED).body(wordService.store(file, wordName));
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasAuthority('ADMIN')")
     public ResponseEntity<?> deleteWord(@PathVariable long id) {
         wordService.deleteWord(id);
         return ResponseEntity.noContent().build();
